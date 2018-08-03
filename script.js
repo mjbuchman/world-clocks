0a7a99eda92b8e7b7177d23c831583f7f8912248
Michael Buchman <mbookie213@gmail.com>
2018-08-03 16:56:56 -0500

    Added functionality for all clocks

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-const HOURHAND = document.querySelector("#hour");
-const MINUTEHAND = document.querySelector("#minute");
-const SECONDHAND = document.querySelector("#second");
+var hourhand = document.querySelectorAll("#hour");
+var minutehand = document.querySelectorAll("#minute");
+var secondhand = document.querySelectorAll("#second");
 
-var date = new Date();
-
-let hr = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
-
-let hrPosition = (hr*360/12)+(min*(360/60)/12);
-let minPosition = (min*360/60)+(sec*(360/60)/60);
-let secPosition = sec*360/60;
-
-function runTheClock() {
-
-    hrPosition = hrPosition+(3/360);
-    minPosition = minPosition+(6/60);
-    secPosition = secPosition+6;
-
-    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
-
+for(i = 0; i < hourhand.length; i++){
+    operateClock(i);
 }
 
-var interval = setInterval(runTheClock, 1000);
+function operateClock(index) {
+    var offset = 0;
+    var date = new Date(new Date().getTime() + offset * 3600 * 1000);
+
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+    let hrPosition = (hr*360/12)+(min*(360/60)/12);
+    let minPosition = (min*360/60)+(sec*(360/60)/60);
+    let secPosition = sec*360/60;
+
+    function runTheClock() {
+
+        hrPosition = hrPosition+(3/360);
+        minPosition = minPosition+(6/60);
+        secPosition = secPosition+6;
+
+        hourhand[index].style.transform = "rotate(" + hrPosition + "deg)";
+        minutehand[index].style.transform = "rotate(" + minPosition + "deg)";
+        secondhand[index].style.transform = "rotate(" + secPosition + "deg)";
+
+    }
+
+    var interval = setInterval(runTheClock, 1000);
+}
